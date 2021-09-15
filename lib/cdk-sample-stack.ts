a1547f0f94532f0e3cb161ce791fafc063284ed2
ito0804takuya <ito0804takuya@gmail.com>
2021-09-15 20:13:19 +0900

    lambda関数作成 追記

--- lib/cdk-sample-stack.ts
+++ lib/cdk-sample-stack.ts
@@ -5,11 +5,11 @@ export class CdkSampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Lamdaリソースをスタックに追加
     const hello = new lambda.Function(this, 'HelloHandler', {
-      runtime: lambda.Runtime.NODEJS_14_X,
-      code: lambda.Code.fromAsset('lambda'),
-      handler: 'hello.handler'
+      runtime: lambda.Runtime.NODEJS_14_X, // 実行環境
+      code: lambda.Code.fromAsset('lambda'), // lamdaディレクトリのコードを読み込む
+      handler: 'hello.handler' // "hello"ファイルの"handler"関数を実行する
     });
   }
 }
