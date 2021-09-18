bb205b6135ba4488d237068a1377dbb8aa0c2898
ito0804takuya <ito0804takuya@gmail.com>
2021-09-18 12:24:43 +0900

    CDK カウンター機能追加(途中/失敗状態)

--- lib/cdk-sample-stack.ts
+++ lib/cdk-sample-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import { HitCounter } from './hitcounter';
 
 export class CdkSampleStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -13,9 +14,13 @@ export class CdkSampleStack extends cdk.Stack {
       handler: 'hello.handler' // "hello"ファイルの"handler"関数を実行する
     });
 
+    const helloWithCounter = new HitCounter(this, 'HelloHitCounter', {
+      downstream: hello
+    });
+
     // hello関数(Lamdaリソース)へのリクエストをプロキシするAPIGatewayを定義
     new apigw.LambdaRestApi(this, 'Endpoint', {
-      handler: hello
+      handler: helloWithCounter.handler
     });
   }
 }
